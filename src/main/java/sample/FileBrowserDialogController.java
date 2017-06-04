8acd97fc204cea90010f76fcc69c0a639df9e552
edleijnse <Rax03Dam>
2017-06-04 20:29:26 +0200

    Directory Dialog implemented

--- src/main/java/sample/FileBrowserDialogController.java
+++ src/main/java/sample/FileBrowserDialogController.java
@@ -4,7 +4,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -13,10 +17,34 @@ public class FileBrowserDialogController implements Initializable{
     @FXML
     private Label label;
 
+    FileChooser fileChooser = new FileChooser();
+    DirectoryChooser directoryChooser = new DirectoryChooser();
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void handleButtonDirectoryChooserAction(ActionEvent event) {
+        System.out.println("Directory Chooser activated");
+        label.setText("Directory Chooser");
+        configuringFileChooser(fileChooser);
+
+        // directoryChooser.setInitialDirectory( fileChooser.showOpenDialog(new Stage()));
+
+        System.out.println("choosen directory: " + directoryChooser.showDialog(new Stage()));
+    }
+
+    private void configuringFileChooser(FileChooser fileChooser) {
+        // Set title for FileChooser
+        fileChooser.setTitle("Select Some Files");
+
+        // Set Initial Directory
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+    }
+
+    private void configuringDirectoryChooser(DirectoryChooser directoryChooser) {
+        // Set title for FileChooser
+        directoryChooser.setTitle("Select Directory");
+
+        // Set Initial Directory
+        directoryChooser.setInitialDirectory(new File(System.getProperty("user.home")));
     }
 
     @Override
